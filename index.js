dab8573d38cf239064f391a67b7a6608286590a2
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-04-04 16:13:22 -0300

    screenshot e dimensoes
    Alem de fazer screenshot por elemento, também permite definir a area do screenshot. É possivel também buscar a resolução do elemento

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const url = 'https://http.cat/';
     //criando nova pagina
     const page = await browser.newPage();
     //navega para pagina
-    await page.goto(url);
+    await page.goto(url, { waitUntil: 'load' });
 
     //screenshot de elemento
     await screenshot_element(page, '#root > div > ul > li:nth-child(2)', 'files/screenshot_element.png');
@@ -18,12 +18,40 @@ const url = 'https://http.cat/';
     //screenshot de determinada area
     await screenshot_crop(page, { x: 10, y: 20, height: 100, width: 100}, 'files/screenshot_crop.png')
 
+    //dimensoes de elemento na pagina
+    const dimensions = await element_dimensions(page, '#root > div > ul > li:nth-child(2)');
+    console.log(dimensions);
+
     //fecha o navegador
     await browser.close();
 })();
 
+//desc: responsavel por devolver altura e largura de determinado elemento na pagina
+//params: (page) instancia da pagina, (string) seletor do elemento
+//return: (obj) altura e alrgura do elemento
+async function element_dimensions(page, selector_in_page){
+    //busca na pagina o elemento
+    const element = await page.$(selector_in_page);
+
+    //se encontrou elemento
+    if(element){
+        //abre um console para executar o trecho abaixo
+        return await page.evaluate((selector_in_page) => {
+            //constante com o elemento
+            const element = document.querySelector(selector_in_page);
+            //retorna as dimensoes
+            return {
+                "height": element.getBoundingClientRect().height, //altura
+                "width": element.getBoundingClientRect().width //largura
+            };
+        }, selector_in_page);// passa para o console uma variavel local
+    }
+    //retorna nulo se nao encontrou elemento
+    return null;
+}
+
 //desc: responsavel por caputrar uma parte do conteudo da pagina recebendo seu identificador como parametro
-//params: (page) instancia da pagina, (string) seletor
+//params: (page) instancia da pagina, (string) seletor do elemento
 //return: nenhum
 async function screenshot_element(page, selector_in_page, file_name){
     let element = await page.$(selector_in_page)
